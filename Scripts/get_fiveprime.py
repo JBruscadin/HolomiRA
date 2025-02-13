729863a05ddb93bc17c21a67ac0c37c6ac390682
JBruscadin <64608861+JBruscadin@users.noreply.github.com>
2025-02-13 17:14:19 -0300

    Update get_fiveprime.py

--- Scripts/get_fiveprime.py
+++ Scripts/get_fiveprime.py
@@ -22,7 +22,7 @@ fasta=f"{fasta_dir}/{sample}.fa"
 b=a.each(pybedtools.featurefuncs.five_prime, upstream, downstream, add_to_name=None, genome=None).saveas(f"{out_dir}/annotation/{sample}/{sample}_cds_fiveprime.gff")
 
 input_gff = f"{out_dir}/annotation/{sample}/{sample}_cds_fiveprime.gff"
-fasta = f"{fasta_dir}/{sample}.fa"
+fasta = f"{out_dir}/annotation/{sample}/{sample}.fna"
 output_fasta = f"{out_dir}/target_fasta/{sample}_filtered.fa"
 warnings_file = "warnings.log"
 
@@ -34,4 +34,4 @@ output_fasta = f"{out_dir}/target_fasta/{sample}_CDS.fa"
 warnings_file = "warnings2.log"
 
 command2 = f"bedtools getfasta -s -fo {output_fasta} -fi {fasta} -bed {input_gff2} 2> {warnings_file}"
-os.system(command2)    
+os.system(command2)
